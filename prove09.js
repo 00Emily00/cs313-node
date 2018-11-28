309511cbdad1b5b2e8dd78415c56394ad729af03
00Emily00 <emyzu97@hotmail.com>
2018-11-28 10:21:07 -0700

    Prove09

--- prove09.js
+++ prove09.js
@@ -13,8 +13,10 @@ function calculateRate(req, res) {
         case "Letters (Metered)":
             if(weight <= 1)
                 cost = .47;
-            else    
-                cost = .47 + 21 * (weight - 1);
+            else(weight > 1 && weight < 3.5)    
+                cost = .47 + .21 * (weight - 1);
+            else
+                document.write("mail is greater than 3.5 oz, use large envelopes")
             break;
         case "Large Envelopes (Flats)":
             if(weight <= 1)
