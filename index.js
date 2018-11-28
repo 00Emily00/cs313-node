eac912fa6a75edf0cfad2c6b98a76cc58270aa99
00Emily00 <emyzu97@hotmail.com>
2018-11-28 09:07:46 -0700

    Prove09

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const calculate = require('prove09.js');
+const calculate = require('./prove09.js');
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
